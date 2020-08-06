28cbc8e1f5c384915011d2a1b4936905cef38f15
Colby Roberts <colbiverse@gmail.com>
2020-08-06 12:25:03 -0700

    fixed logistic_regression

--- analysis/logistic_regression.py
+++ analysis/logistic_regression.py
@@ -3,39 +3,31 @@ import sys
 sys.path.append('src')
 from polynomial_regressor import PolynomialRegressor
 
-print('\npoly_regess')
 data = [(10, 0.05), (100, 0.35), (1000, 0.95)]
-poly_regress = PolynomialRegressor()
-poly_regress.ingest_data(data)
-poly_regress.solve_coefficients()
 
-def function(x, beta_0, beta_1): #for testing
+def function(x, beta_0, beta_1):  #for testing
     return 1 / (1 + math.e ** (beta_0 + beta_1 * x))
 
 # 0.05 = 1/(1 + e ^ (beta_0 + beta_1 * 10))
 # 0.35 = 1/(1 + e ^ (beta_0 + beta_1 * 100))
-# 0.75 = 1/(1 + e ^ (beta_0 + beta_1 * 1000))
+# 0.95 = 1/(1 + e ^ (beta_0 + beta_1 * 1000))
 
 # beta_0 + beta_1 * 10 = ln(1/0.05 - 1)
 # beta_0 + beta_1 * 100 = ln(1/0.35 - 1)
-# beta_0 + beta_1 * 1000 = ln(1/0.75 - 1)
+# beta_0 + beta_1 * 1000 = ln(1/0.95 - 1)
 
 # [[1, 10],     [[beta_0],   [[ln(1/0.05 - 1)]
 #  [1, 100], *   [beta_1]] =  [ln(1/0.35 - 1)]
-#  [1, 1000]]                 [ln(1/0.75 - 1)]]
+#  [1, 1000]]                 [ln(1/0.95 - 1)]]
 
 print('new_poly_regress')
-new_data = [[1, 10, math.log(1/0.05 - 1)], [1, 100, math.log(1/0.35 - 1)], [1, 1000, math.log(1/0.75 - 1)]]
+new_data = [[1, 10, math.log(1/0.05 - 1)], [1, 100, math.log(1/0.35 - 1)], [1, 1000, math.log(1/0.95 - 1)]]
 
 new_poly_regress = PolynomialRegressor()
 new_poly_regress.ingest_data(new_data)
 new_poly_regress.solve_coefficients()
 #print('poly_regress coeffs', poly_regress.coefficients)
 
-print('probability of a player who has played 500 hours (poly_regress):', function(500, poly_regress.coefficients[0], poly_regress.coefficients[1]))
+print('\nprobability of a player who has played 500 hours:', function(500, new_poly_regress.coefficients[0], new_poly_regress.coefficients[1]))
 
-print('\nprobability of a player who has played 500 hours (new_poly_regress):', function(500, new_poly_regress.coefficients[0], new_poly_regress.coefficients[1]))
-
-print('\nhow long an average player practices (poly_regress):', (math.log(1/0.5 - 1) - poly_regress.coefficients[0]) / poly_regress.coefficients[1])
-
-print('\nhow long an average player practices (new_poly_regress):', (math.log(1/0.5 - 1) - new_poly_regress.coefficients[0]) / new_poly_regress.coefficients[1])
+print('\nhow long an average player practices:', (math.log(1/0.5 - 1) - new_poly_regress.coefficients[0]) / new_poly_regress.coefficients[1])
