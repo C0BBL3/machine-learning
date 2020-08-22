e4b0b41fb8fd304bab0116533b50caad903ef6c2
Colby Roberts <colbiverse@gmail.com>
2020-08-22 14:03:40 -0700

    Update logistic-regression-space-empires.py

--- analysis/logistic-regression-space-empires.py
+++ analysis/logistic-regression-space-empires.py
@@ -13,12 +13,12 @@ def logistic_regression_function(x, beta_0, beta_1):  # for testing
 
 
 print('poly_regress')
-new_data = [(0, 0.01, math.log(1 / 0.01 - 1)), (0, 0.01, math.log(1 / 0.01 - 1)), (0, 0.05, math.log(1 / 0.05 - 1)), (10, 0.02, math.log(1 / 0.02 - 1)),
-            (10, 0.15, math.log(1 / 0.15 - 1)),(50, 0.12, math.log(1 / 0.12 - 1)), (50, 0.28, math.log(1 / 0.28 - 1)), (73, 0.03, math.log(1 / 0.03 - 1)), 
-            (80, 0.10, math.log(1 / 0.10 - 1)), (115, 0.06, math.log(1 / 0.06 - 1)), (150, 0.12, math.log(1 / 0.12 - 1)), (170, 0.30, math.log(1 / 0.3 - 1)), 
-            (175, 0.24, math.log(1 / 0.24 - 1)), (198, 0.26, math.log(1 / 0.26 - 1)), (212, 0.25, math.log(1 / 0.25 - 1)), (232, 0.32, math.log(1 / 0.32 - 1)),
-            (240, 0.45, math.log(1 / 0.45 - 1)), (381, 0.93, math.log(1 / 0.93 - 1)), (390, 0.87, math.log(1 / 0.87 - 1)), (402, 0.95, math.log(1 / 0.95 - 1)),
-            (450, 0.98, math.log(1 / 0.98 - 1)), (450, 0.85, math.log(1 / 0.85 - 1)), (450, 0.95, math.log(1 / 0.95 - 1)), (460, 0.91, math.log(1 / 0.91 - 1)), (500, 0.95, math.log(1 / 0.95 - 1))]
+new_data = [(0, math.log(1 / 0.01 - 1)), (0, math.log(1 / 0.01 - 1)), (0, math.log(1 / 0.05 - 1)), (10, math.log(1 / 0.02 - 1)),
+            (10, math.log(1 / 0.15 - 1)),(50, math.log(1 / 0.12 - 1)), (50, math.log(1 / 0.28 - 1)), (73, math.log(1 / 0.03 - 1)), 
+            (80, math.log(1 / 0.10 - 1)), (115, math.log(1 / 0.06 - 1)), (150, math.log(1 / 0.12 - 1)), (170, math.log(1 / 0.3 - 1)), 
+            (175, math.log(1 / 0.24 - 1)), (198, math.log(1 / 0.26 - 1)), (212, math.log(1 / 0.25 - 1)), (232, math.log(1 / 0.32 - 1)),
+            (240, math.log(1 / 0.45 - 1)), (381, math.log(1 / 0.93 - 1)), (390, math.log(1 / 0.87 - 1)), (402, math.log(1 / 0.95 - 1)),
+            (450, math.log(1 / 0.98 - 1)), (450, math.log(1 / 0.85 - 1)), (450, math.log(1 / 0.95 - 1)), (460, math.log(1 / 0.91 - 1)), (500, math.log(1 / 0.95 - 1))]
 
 poly_regress=PolynomialRegressor()
 poly_regress.ingest_data(new_data)
