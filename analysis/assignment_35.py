e10f6aff65c31aa5c756a4ab9a8f04060ccacb8a
Colby Roberts <colbiverse@gmail.com>
2020-08-26 10:14:36 -0700

    Update assignment_35.py

--- analysis/assignment_35.py
+++ analysis/assignment_35.py
@@ -1,7 +1,7 @@
 import math
 import sys
 sys.path.append('src')
-from polynomial_regressor import PolynomialRegressor
+from matrix_class import Matrix
 '''
 data = [[1, 0, 0, 0, 0, 1],
         [1, 0, 0, 1, 0, 1],
@@ -39,19 +39,48 @@ data = [[1,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0, 1],
 
 
 def logistic_regression_function(x, betas):  # for testing
-    return 1 / (1 + math.e ** sum([beta * x for beta in betas]))
+      return 1 / (1 + math.e ** sum([beta * x for beta in betas]))
+
+
+def solve_coefficients(data):
+      X_data = []
+      Y_data = []
+      for arr in data:
+            X_data.append(arr[:-1])
+            Y_data.append([arr[-1]])
+      X_matrix = Matrix(elements=X_data)
+      print('\nX_matrix.elements', X_matrix.elements,)
+      Y_matrix = Matrix(elements=Y_data)
+      print('Y_matrix.elements', Y_matrix.elements)
+      X_transpose = X_matrix.transpose()  # xT
+      #print('xT', X_transpose.elements)
+      X_transpose_times_X = X_transpose @ X_matrix  # xT * x
+      #print('(xT * x)', X_transpose_times_X.elements)
+      X_transpose_times_X_inverse = X_transpose_times_X.inverse()
+      #print('(xT * x)^-1', X_transpose_times_X_inverse.elements)
+      X_transpose_times_X_inverse_times_X_transpose = X_transpose_times_X_inverse @ X_transpose
+      #print('(xT * x)^-1 * xT',
+           # X_transpose_times_X_inverse_times_X_transpose.elements)
+      result = X_transpose_times_X_inverse_times_X_transpose @ Y_matrix
+      # (xT * x)^-1 * xT * y
+      #print('(xT * x)^-1 * xT * y', result.elements, '\n')
+      coefficients = []
+      for results in result.elements:
+            if results != result.elements:
+                  coefficients.append(results[0])
+
+      print('Coeffs', coefficients, '\n')
+      return coefficients
 
 print('poly_regress')
-poly_regress = PolynomialRegressor()
-poly_regress.ingest_data(data)
-poly_regress.solve_coefficients()
-print('poly_regress coeffs', poly_regress.coefficients)  # 2.66
-print('no ingredients:', poly_regress.coefficients[0])  # 0.59
-print('mayo only:', poly_regress.coefficients[0] + poly_regress.coefficients[3])  # 0.59
-print('mayo and jelly:', poly_regress.coefficients[0] + poly_regress.coefficients[3] + poly_regress.coefficients[4])  # 0.07
-print('5 slices beef + mayo:', poly_regress.coefficients[0] + 5 *
-      poly_regress.coefficients[1] + poly_regress.coefficients[3])  # 7.64
-print('5 tbsp pb + jelly:', poly_regress.coefficients[0] + 5 *
-      poly_regress.coefficients[1] + poly_regress.coefficients[4])  # 8.94
-print('5 slices beef + 5 tbsp pb + mayo + jelly:', poly_regress.coefficients[0] + 5 *
-      poly_regress.coefficients[1] + 5 * poly_regress.coefficients[2] + poly_regress.coefficients[3] + poly_regress.coefficients[4])  # 0.02
+coefficients = solve_coefficients(data)
+print('poly_regress coeffs', coefficients)  # 2.66
+print('no ingredients:', coefficients[0])  # 0.59
+print('mayo only:', coefficients[0] + coefficients[3])  # 0.59
+print('mayo and jelly:', coefficients[0] + coefficients[3] + coefficients[4])  # 0.07
+print('5 slices beef + mayo:', coefficients[0] + 5 *
+      coefficients[1] + coefficients[3])  # 7.64
+print('5 tbsp pb + jelly:', coefficients[0] + 5 *
+      coefficients[1] + coefficients[4])  # 8.94
+print('5 slices beef + 5 tbsp pb + mayo + jelly:', coefficients[0] + 5 *
+      coefficients[1] + 5 * coefficients[2] + coefficients[3] + coefficients[4])  # 0.02
