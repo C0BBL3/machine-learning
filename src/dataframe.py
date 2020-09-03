6716adf5de89859203b54f5a02b97ca355854f5d
Colby Roberts <colbiverse@gmail.com>
2020-09-03 16:14:31 -0700

    added graph.append_pairwise_interactions()

--- src/dataframe.py
+++ src/dataframe.py
@@ -18,6 +18,14 @@ class DataFrame:
 
     def apply(self, column, function):
         self.data_dict[column] = [function(value) for value in self.data_dict[column]]
+
+    def append_pairwise_interactions(self):
+        new_cols, new_cols_in_matrix, length_of_old_cols = [], [], len(self.columns)
+        new_cols = [key_1 + '_' + key_2 for j, (key_2, col_2) in enumerate(self.data_dict.items()) for i, (key_1, col_1) in enumerate(self.data_dict.items()) if j > i]
+        new_cols_in_matrix = [[col_1[i] * col_2[i] for i in range(0, len(col_1))] for j, (key_2, col_2) in enumerate(self.data_dict.items()) for i, (key_1, col_1) in enumerate(self.data_dict.items()) if j > i]
+        self.columns += new_cols
+        for i, col in enumerate(new_cols_in_matrix): self.data_dict[self.columns[i + length_of_old_cols]] = col
+        self.array = self.to_array()
         
 
 
--- tests/test_dataframe.py
+++ tests/test_dataframe.py
@@ -22,8 +22,8 @@ print("    Testing DataFrame 1's columns")
 assert df1.columns == ['Pete', 'John', 'Sara'], "DataFrame 1's columns were not right, they should be ['Pete', 'John', 'Sara'], but were {}".format(df1.columns)
 print("    DataFrame 1's columns Passed!!!\n")
 
-df1.filter_columns(['Sara', 'Pete'])
 df2 = df1
+df2.filter_columns(['Sara', 'Pete'])
 print("    Testing DataFrame 2's filter_columns")
 assert df2.to_array() == [[3, 1], [1, 0], [4, 1], [0, 0]], "DataFrame 2's filter_columns were not right, they should be [[3, 1], [1, 0], [4, 1], [0, 0]], but were {}".format(df2.to_array())
 print("    DataFrame 2's filter_columns Passed!!!\n")
@@ -40,5 +40,16 @@ df3.apply('John', multiply_by_4)
 print("    Testing DataFrame 3's apply()")
 assert df3.to_array() == [[1, 8, 3], [0, 4, 1], [1, 0, 4], [0, 8, 0]], "DataFrame 2's apply() was not right, it should be [[1, 8, 3], [0, 4, 1], [1, 0, 4], [0, 8, 0]], but was {}".format(df2.to_array())
 print("    DataFrame 2's apply() Passed!!!\n")
+
+df4 = DataFrame(data_dict, column_order=['Pete', 'John', 'Sara'])
+df4.to_array()
+df4.append_pairwise_interactions()
+print("    Testing DataFrame 4's columns")
+assert df4.columns == ['Pete', 'John', 'Sara', 'Pete_John', 'Pete_Sara', 'John_Sara'], "DataFrame 4's columns were not right, they should be['Pete', 'John', 'Sara', 'Pete_John', 'Pete_Sara', 'John_Sara'], but were {}".format(df4.columns)
+print("    DataFrame 4's columns Passed!!!\n")
+
+print("    Testing DataFrame 4's to_array()")
+assert df4.to_array() == [[1, 2, 3, 2, 3, 6], [0, 1, 1, 0, 0, 1], [1, 0, 4, 0, 4, 0], [0, 2, 0, 0, 0, 0]], "DataFrame 4's to_array() was not right, it should be [[1, 2, 3, 2, 3, 6], [0, 1, 1, 0, 0, 1], [1, 0, 4, 0, 4, 0], [0, 2, 0, 0, 0, 0]], but was {}".format(df4.to_array())
+print("    DataFrame 4's to_array() Passed!!!\n")
 
 print('ALL TESTS PASS!!!!!')
