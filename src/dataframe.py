c69864fb299855b03b1187d0608bedbfed05d705
Colby Roberts <colbiverse@gmail.com>
2020-09-06 14:00:24 -0700

    Update dataframe.py

--- src/dataframe.py
+++ src/dataframe.py
@@ -4,8 +4,8 @@ class DataFrame:
     def __init__(self, data_dict, column_order):
         self.data_dict = {}
         self.columns = column_order
-        for person in self.columns:
-            self.data_dict[person] = data_dict[person]
+        for key in self.columns:
+            self.data_dict[key] = data_dict[key]
 
     def to_array(self):
         self.array = [value for _, value in self.data_dict.items()]
@@ -21,12 +21,38 @@ class DataFrame:
 
     def append_pairwise_interactions(self):
         new_cols, new_cols_in_matrix, length_of_old_cols = [], [], len(self.columns)
-        new_cols = [key_1 + '_' + key_2 for j, (key_2, col_2) in enumerate(self.data_dict.items()) for i, (key_1, col_1) in enumerate(self.data_dict.items()) if j > i]
-        new_cols_in_matrix = [[col_1[i] * col_2[i] for i in range(0, len(col_1))] for j, (key_2, col_2) in enumerate(self.data_dict.items()) for i, (key_1, col_1) in enumerate(self.data_dict.items()) if j > i]
+        for j, (key_2, col_2) in enumerate(self.data_dict.items()):
+            for i, (key_1, col_1) in enumerate(self.data_dict.items()):
+                if j > i:
+                    new_cols.append(key_1 + '_' + key_2)
+                    new_cols_in_matrix.append([col_1[i] * col_2[i] for i in range(0, len(col_1))])
         self.columns += new_cols
         for i, col in enumerate(new_cols_in_matrix): self.data_dict[self.columns[i + length_of_old_cols]] = col
         self.array = self.to_array()
-        
+
+    def append_columns(self, dictionary):
+        for col, value in dictionary.items():
+            self.columns.append(col)
+            self.data_dict[col] = value
+
+    def remove_columns(self, columns):
+        self.columns = [col for col, _ in self.data_dict.items() if col not in columns]
+        self.filter_columns(self.columns)
+
+    def create_dummy_variables(self, stoopid_column):
+        new_cols, rows = self.get_unique_dummy_columns(stoopid_column)
+        self.remove_columns(stoopid_column)
+        self.append_columns({col: rows[i] for i, col in enumerate(new_cols)})
+
+    def get_unique_dummy_columns(self, stoopid_column):
+        new_cols, rows = [], []
+        for value_1 in self.data_dict[stoopid_column]:
+            if str(stoopid_column) + '-' + str(value_1) not in new_cols:
+                new_cols.append(str(stoopid_column) + '_' + str(value_1))
+                rows.append([1 if value_1 == value_2  else 0 for value_2 in self.data_dict[stoopid_column]])
+
+        return new_cols, rows
+
 
 
 
--- tests/test_dataframe.py
+++ tests/test_dataframe.py
@@ -32,6 +32,8 @@ print("    Testing DataFrame 2's columns")
 assert df2.columns == ['Sara', 'Pete'], "DataFrame 2's columns were not right, they should be ['Sara', 'Pete'], but were {}".format(df2.columns)
 print("    DataFrame 2's columns Passed!!!\n")
 
+print('\n--------------------------------\n')
+
 def multiply_by_4(x):
     return 4 * x
 
@@ -45,11 +47,43 @@ df4 = DataFrame(data_dict, column_order=['Pete', 'John', 'Sara'])
 df4.to_array()
 df4.append_pairwise_interactions()
 print("    Testing DataFrame 4's columns")
-assert df4.columns == ['Pete', 'John', 'Sara', 'Pete_John', 'Pete_Sara', 'John_Sara'], "DataFrame 4's columns were not right, they should be['Pete', 'John', 'Sara', 'Pete_John', 'Pete_Sara', 'John_Sara'], but were {}".format(df4.columns)
+assert df4.columns == ['Pete', 'John', 'Sara', 'Pete_John', 'Pete_Sara', 'John_Sara'], "DataFrame 4's columns were not right, they should be ['Pete', 'John', 'Sara', 'Pete_John', 'Pete_Sara', 'John_Sara'], but were {}".format(df4.columns)
 print("    DataFrame 4's columns Passed!!!\n")
 
 print("    Testing DataFrame 4's to_array()")
 assert df4.to_array() == [[1, 2, 3, 2, 3, 6], [0, 1, 1, 0, 0, 1], [1, 0, 4, 0, 4, 0], [0, 2, 0, 0, 0, 0]], "DataFrame 4's to_array() was not right, it should be [[1, 2, 3, 2, 3, 6], [0, 1, 1, 0, 0, 1], [1, 0, 4, 0, 4, 0], [0, 2, 0, 0, 0, 0]], but was {}".format(df4.to_array())
 print("    DataFrame 4's to_array() Passed!!!\n")
+
+print('\n--------------------------------\n')
+
+data_dict_2 = {
+    'id': [1, 2, 3, 4],
+    'color': ['blue', 'yellow', 'green', 'yellow']
+}
+
+df5 = DataFrame(data_dict_2, column_order=['id', 'color'])
+df5.create_dummy_variables('color')
+print("    Testing DataFrame 5's create_dummy_variables()")
+assert df5.columns == ['id', 'color_blue', 'color_yellow', 'color_green'], "DataFrame 5's columns were not right, they should be ['id', 'color_blue', 'color_yellow', 'color_green'], but were {}".format(df5.columns)
+print("    DataFrame 5's create_dummy_variables() Passed!!!\n")
+
+df5.remove_columns(['id', 'color_yellow'])
+print("    Testing DataFrame 5's remove_columns()")
+assert df5.columns == ['color_blue', 'color_green'], "DataFrame 5's columns were not right, they should be ['color_blue', 'color_green'], but were {}".format(df5.columns)
+print("    DataFrame 5's remove_columns() Passed!!!\n")
+
+df5.append_columns({
+    'name': ['Anna', 'Bill', 'Cayden', 'Daphnie'],
+    'letter': ['a', 'b', 'c', 'd']
+})
+print("    Testing DataFrame 5's append_columns()")
+assert df5.columns == ['color_blue', 'color_green', 'name', 'letter'], "DataFrame 5's columns were not right, they should be ['color_blue', 'color_green', 'name', 'letter'], but were {}".format(df5.columns)
+print("    DataFrame 5's append_columns() Passed!!!\n")
+
+
+print("    Testing DataFrame 5's to_array()")
+assert df5.to_array() == [[1, 0, 'Anna', 'a'], [0, 0, 'Bill', 'b'], [0, 1, 'Cayden', 'c'], [0, 0, 'Daphnie', 'd']], "DataFrame 5's to_array() was not right, it should be [[1, 0, 'Annna', 'a'], [0, 0, 'Bill', 'b'], [0, 1, 'Cayden', 'c'], [0, 0, 'Daphnie', 'd']], but was {}".format(df5.to_array())
+print("    DataFrame 5's to_array() Passed!!!\n")
+
 
 print('ALL TESTS PASS!!!!!')
